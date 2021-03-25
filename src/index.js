70de63aaf5f5cd0923e7e3c19830381da9d84f57
Johan von Boer <johan.von.boer@umu.se>
2021-03-25 12:46:16 +0100

    rm some old code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-const express = require('express');
-const bodyParser = require('body-parser');
-//const WebSocket = require('ws');
 const fs = require('fs');
 const SessionManager = require('./SessionManager.class.js');
 const SessionProxyServer = require('./SessionProxyServer.class');
@@ -15,87 +10,14 @@ class Application {
     this.hsApiAccessToken = process.env.HS_API_ACCESS_TOKEN;
     this.gitlabAccessToken = process.env.GIT_API_ACCESS_TOKEN;
     this.logLevel = process.env.LOG_LEVEL.toUpperCase();
-    //this.expressApp = express();
-    //this.expressApp.use(bodyParser.urlencoded({ extended: true }));
     
     this.sessMan = new SessionManager(this);
 
-    //this.simpleTestServer();
     this.sessProxyServer = new SessionProxyServer(this);
     this.addLog("SessionProxyServer started at port "+this.sessProxyServer.port);
     this.apiServer = new ApiServer(this);
     this.addLog("ApiServer started at port "+this.apiServer.port);
-    
   }
-
-  /*
-  simpleTestServer() {
-    this.addLog("Starting simple test server");
-    var proxy = new httpProxy.createProxyServer({
-      target: {
-        host: 'jupyter',
-        port: 8888
-      }
-    });
-    
-    let app = express();
-    app.use(bodyParser.urlencoded({ extended: true }));
-    //let proxyServer = app.listen(80);
-    
-    app.all('/*', (req, res) => {
-      proxy.web(req, res);
-    });
-    
-    var proxyServer = http.createServer(app);
-    
-    proxyServer.on('request', (req, res) => {
-      console.log("Proxy http req");
-      proxy.web(req, res);
-    });
-    proxyServer.on('upgrade', (req, socket, head) => {
-      console.log("Proxy ws req");
-      proxy.ws(req, socket, head);
-    });
-    
-    let s = proxyServer.listen(80);
-    
-    this.addLog("Proxy online");
-  }
-  */
-
-  /*
-  trafficDivider(req, res, next) {
-      this.addLog("Request: "+req.url);
-      if(req.headers.hs_api_access_token) {
-        if(!this.checkApiAccessCode(req)) { //Requests to the approuter API must always include the API access code, which should be held by the webapi service
-          res.sendStatus(401);
-          return false;
-        }
-        else {
-          next();
-        }
-      }
-      else {
-        this.sessMan.routeToApp(req, res);
-      }
-  }
-
-  trafficDividerWs(req, socket, head) {
-    this.addLog("WS Request: "+req.url);
-    if(req.headers.hs_api_access_token) {
-      if(!this.checkApiAccessCode(req)) { //Requests to the approuter API must always include the API access code, which should be held by the webapi service
-        res.sendStatus(401);
-        return false;
-      }
-      else {
-        next();
-      }
-    }
-    else {
-      this.sessMan.routeToAppWs(req, socket, head);
-    }
-  }
-  */
 
   addLog(msg, level = 'info') {
     let levelMsg = new String(level).toUpperCase();
@@ -126,4 +48,3 @@ class Application {
 }
 
 const application = new Application();
-//application.startServer();
