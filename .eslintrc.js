9334943ace81b7b5d5397a80a9727aeb277c8948
Johan von Boer <johan.von.boer@umu.se>
2026-01-26 11:06:24 +0100

    removed

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: "@babel/eslint-parser",
-  env: { node: true, es2020: true },
-  extends: ['eslint:recommended', 'prettier'],
-  parserOptions: { ecmaVersion: 2020, requireConfigFile: false },
-  rules: {
-    'no-unused-vars': 'off'
-  }
-};
